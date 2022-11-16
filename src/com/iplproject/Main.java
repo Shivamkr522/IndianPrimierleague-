ecedae6c397d411ccd02585090f4ec9384bde17a
Shivamkr522 <work.shivam522@gmail.com>
2022-11-16 12:35:04 +0530

    Final Commit

--- src/com/iplproject/Main.java
+++ src/com/iplproject/Main.java
@@ -2,50 +2,48 @@ package com.iplproject;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.util.*;
 
 
 public class Main {
 
-    public static final int SET_ID = 0;
-    public static final int SET_SEASON = 1;
-    public static final int SET_CITY = 2;
-    public static final int SET_DATE = 3;
-    public static final int SET_TEAM1 = 4;
-    public static final int SET_TEAM2 = 5;
-    public static final int SET_TOSS_WINNER = 6;
-    public static final int SET_TOSS_DECISION = 7;
-    public static final int SET_RESULT = 8;
-    public static final int SET_DL_APPLIED = 9;
-    public static final int SET_WINNER = 10;
-    public static final int SET_WIN_BY_RUNS = 11;
-    public static final int SET_WIN_BY_WICKETS = 12;
-    public static final int SET_PLAYER_OF_MATCH = 13;
-    public static final int SET_VENUE = 14;
-    public static final int SET_UMPIRE1 = 15;
-    public static final int SET_UMPIRE2 = 16;
-    public static final int SET_DELIVERY_INNING = 1;
-    public static final int SET_DELIVERY_BATTING_TEAM = 2;
-    public static final int SET_DELIVERY_BOWLING_TEAM = 3;
-    public static final int SET_DELIVERY_OVER = 4;
-    public static final int SET_DELIVERY_BALL = 5;
-    public static final int SET_DELIVERY_BATSMAN = 6;
-    public static final int SET_DELIVERY_NON_STRIKER = 7;
-    public static final int SET_DELIVERY_BOWLER = 8;
-    public static final int SET_DELIVERY_IS_SUPER_OVER = 9;
-    public static final int SET_DELIVERY_WIDE_RUN = 10;
-    public static final int SET_DELIVERY_BYE_RUNS = 11;
-    public static final int SET_DELIVERY_LEG_BYE_RUNS = 12;
-    public static final int SET_DELIVERY_NO_BALL_RUNS = 13;
-    public static final int SET_DELIVERY_PENALTY_RUNS = 14;
-    public static final int SET_DELIVERY_BATSMAN_RUNS = 15;
-    public static final int SET_DELIVERY_EXTRA_RUNS = 16;
-    public static final int SET_DELIVERY_TOTAL_RUNS = 17;
-    public static final int SET_DELIVERY_PLAYER_DISMISSED = 18;
-    public static final int SET_DELIVERY_DISMISSAL_KIND = 19;
-    public static final int SET_DELIVERY_FIELDER = 20;
+    public static final int ID = 0;
+    public static final int MATCH_SEASON = 1;
+    public static final int MATCH_CITY = 2;
+    public static final int MATCH_DATE = 3;
+    public static final int MATCH_TEAM1 = 4;
+    public static final int MATCH_TEAM2 = 5;
+    public static final int MATCH_TOSS_WINNER = 6;
+    public static final int MATCH_TOSS_DECISION = 7;
+    public static final int MATCH_RESULT = 8;
+    public static final int MATCH_DL_APPLIED = 9;
+    public static final int MATCH_WINNER = 10;
+    public static final int MATCH_WIN_BY_RUNS = 11;
+    public static final int MATCH_WIN_BY_WICKETS = 12;
+    public static final int MATCH_PLAYER_OF_MATCH = 13;
+    public static final int MATCH_VENUE = 14;
+    public static final int MATCH_UMPIRE1 = 15;
+    public static final int MATCH_UMPIRE2 = 16;
+    public static final int DELIVERY_INNING = 1;
+    public static final int DELIVERY_BATTING_TEAM = 2;
+    public static final int DELIVERY_BOWLING_TEAM = 3;
+    public static final int DELIVERY_OVER = 4;
+    public static final int DELIVERY_BALL = 5;
+    public static final int DELIVERY_BATSMAN = 6;
+    public static final int DELIVERY_NON_STRIKER = 7;
+    public static final int DELIVERY_BOWLER = 8;
+    public static final int DELIVERY_IS_SUPER_OVER = 9;
+    public static final int DELIVERY_WIDE_RUN = 10;
+    public static final int DELIVERY_BYE_RUNS = 11;
+    public static final int DELIVERY_LEG_BYE_RUNS = 12;
+    public static final int DELIVERY_NO_BALL_RUNS = 13;
+    public static final int DELIVERY_PENALTY_RUNS = 14;
+    public static final int DELIVERY_BATSMAN_RUNS = 15;
+    public static final int DELIVERY_EXTRA_RUNS = 16;
+    public static final int DELIVERY_TOTAL_RUNS = 17;
+    public static final int DELIVERY_PLAYER_DISMISSED = 18;
+    public static final int DELIVERY_DISMISSAL_KIND = 19;
+    public static final int DELIVERY_FIELDER = 20;
 
     public static void main(String[] args) {
 
@@ -108,8 +106,7 @@ public class Main {
             double findOvers = (double)findOver/6;
             double precisionNumber = (double)findRestBalls/6;
             findOvers+=precisionNumber;
-            double finalOver = BigDecimal.valueOf(findOvers).setScale(2, RoundingMode.HALF_UP).doubleValue();
-            reducedOverForBowler.put((String) storeOver.getKey(),finalOver);
+            reducedOverForBowler.put((String) storeOver.getKey(),findOvers);
         }
         for(Map.Entry finalStore : storeBallerRun.entrySet()){
             double over = reducedOverForBowler.get(finalStore.getKey());
@@ -117,11 +114,11 @@ public class Main {
             double economy = runs/over;
             economicalBowler.put((String) finalStore.getKey(),economy);
         }
-        double maximum = Collections.min(economicalBowler.values());
+        double minimum = Collections.min(economicalBowler.values());
 
         for(Map.Entry mp : economicalBowler.entrySet()){
-            if(mp.getValue().equals(maximum))
-                System.out.println(mp.getKey()+ " "+ maximum);
+            if(mp.getValue().equals(minimum))
+                System.out.println(mp.getKey()+ " "+String.format("%.2f",minimum) );
         }
     }
 
@@ -185,40 +182,40 @@ public class Main {
     private static List<Delivery> readDeliveriesData() {
         List<Delivery> deliveries = new ArrayList<>();
         try{
-            BufferedReader readDeliveries = new BufferedReader(new FileReader("/home/shivam/Project1/deliveries.csv"));
+            BufferedReader readDeliveries = new BufferedReader(new FileReader("Data/deliveries.csv"));
             String line;
             while((line = readDeliveries.readLine())!=null)
             {
                 List<String> lineDeliveries = new ArrayList<>(Arrays.asList(line.split(",")));
                 Delivery delivery = new Delivery();
-                delivery.setMatchId(lineDeliveries.get(SET_ID));
-                delivery.setInning(lineDeliveries.get(SET_DELIVERY_INNING));
-                delivery.setBattingTeam(lineDeliveries.get(SET_DELIVERY_BATTING_TEAM));
-                delivery.setBowlingTeam(lineDeliveries.get(SET_DELIVERY_BOWLING_TEAM));
-                delivery.setOver(lineDeliveries.get(SET_DELIVERY_OVER));
-                delivery.setBall(lineDeliveries.get(SET_DELIVERY_BALL));
-                delivery.setBatsman(lineDeliveries.get(SET_DELIVERY_BATSMAN));
-                delivery.setNonStriker(lineDeliveries.get(SET_DELIVERY_NON_STRIKER));
-                delivery.setBowler(lineDeliveries.get(SET_DELIVERY_BOWLER));
-                delivery.setIsSuperOver(lineDeliveries.get(SET_DELIVERY_IS_SUPER_OVER));
-                delivery.setWideRuns(lineDeliveries.get(SET_DELIVERY_WIDE_RUN));
-                delivery.setByeRuns(lineDeliveries.get(SET_DELIVERY_BYE_RUNS));
-                delivery.setLegByeRuns(lineDeliveries.get(SET_DELIVERY_LEG_BYE_RUNS));
-                delivery.setNoballRuns(lineDeliveries.get(SET_DELIVERY_NO_BALL_RUNS));
-                delivery.setPenaltyRuns(lineDeliveries.get(SET_DELIVERY_PENALTY_RUNS));
-                delivery.setBatsmanRuns(lineDeliveries.get(SET_DELIVERY_BATSMAN_RUNS));
-                delivery.setExtraRuns(lineDeliveries.get(SET_DELIVERY_EXTRA_RUNS));
-                delivery.setTotalRuns(lineDeliveries.get(SET_DELIVERY_TOTAL_RUNS));
-                if(lineDeliveries.size()>SET_DELIVERY_TOTAL_RUNS+1)
-                    delivery.setPlayerDismissed(lineDeliveries.get(SET_DELIVERY_PLAYER_DISMISSED));
+                delivery.setMatchId(lineDeliveries.get(ID));
+                delivery.setInning(lineDeliveries.get(DELIVERY_INNING));
+                delivery.setBattingTeam(lineDeliveries.get(DELIVERY_BATTING_TEAM));
+                delivery.setBowlingTeam(lineDeliveries.get(DELIVERY_BOWLING_TEAM));
+                delivery.setOver(lineDeliveries.get(DELIVERY_OVER));
+                delivery.setBall(lineDeliveries.get(DELIVERY_BALL));
+                delivery.setBatsman(lineDeliveries.get(DELIVERY_BATSMAN));
+                delivery.setNonStriker(lineDeliveries.get(DELIVERY_NON_STRIKER));
+                delivery.setBowler(lineDeliveries.get(DELIVERY_BOWLER));
+                delivery.setIsSuperOver(lineDeliveries.get(DELIVERY_IS_SUPER_OVER));
+                delivery.setWideRuns(lineDeliveries.get(DELIVERY_WIDE_RUN));
+                delivery.setByeRuns(lineDeliveries.get(DELIVERY_BYE_RUNS));
+                delivery.setLegByeRuns(lineDeliveries.get(DELIVERY_LEG_BYE_RUNS));
+                delivery.setNoballRuns(lineDeliveries.get(DELIVERY_NO_BALL_RUNS));
+                delivery.setPenaltyRuns(lineDeliveries.get(DELIVERY_PENALTY_RUNS));
+                delivery.setBatsmanRuns(lineDeliveries.get(DELIVERY_BATSMAN_RUNS));
+                delivery.setExtraRuns(lineDeliveries.get(DELIVERY_EXTRA_RUNS));
+                delivery.setTotalRuns(lineDeliveries.get(DELIVERY_TOTAL_RUNS));
+                if(lineDeliveries.size()> DELIVERY_TOTAL_RUNS +1)
+                    delivery.setPlayerDismissed(lineDeliveries.get(DELIVERY_PLAYER_DISMISSED));
                 else
                     delivery.setPlayerDismissed("");
-                if(lineDeliveries.size()>SET_DELIVERY_PLAYER_DISMISSED+1)
-                    delivery.setDismissalKind(lineDeliveries.get(SET_DELIVERY_DISMISSAL_KIND));
+                if(lineDeliveries.size()> DELIVERY_PLAYER_DISMISSED +1)
+                    delivery.setDismissalKind(lineDeliveries.get(DELIVERY_DISMISSAL_KIND));
                 else
                     delivery.setDismissalKind("");
-                if(lineDeliveries.size()>SET_DELIVERY_DISMISSAL_KIND+1)
-                    delivery.setFielder(lineDeliveries.get(SET_DELIVERY_FIELDER));
+                if(lineDeliveries.size()> DELIVERY_DISMISSAL_KIND +1)
+                    delivery.setFielder(lineDeliveries.get(DELIVERY_FIELDER));
                 else
                     delivery.setFielder("");
                 deliveries.add(delivery);
@@ -233,34 +230,34 @@ public class Main {
     private static List<Match> readMatchesData() {
         List<Match> matches = new ArrayList<>();
         try{
-            BufferedReader readMatches = new BufferedReader(new FileReader("/home/shivam/Project1/matches.csv"));
+            BufferedReader readMatches = new BufferedReader(new FileReader("Data/matches.csv"));
             String line;
             while((line=readMatches.readLine())!=null){
                 List<String> lineMatches= new ArrayList<>(Arrays.asList(line.split(",")));
                 Match match = new Match();
-                match.setId(lineMatches.get(SET_ID));
-                match.setSeason(lineMatches.get(SET_SEASON));
-                match.setCity(lineMatches.get(SET_CITY));
-                match.setDate(lineMatches.get(SET_DATE));
-                match.setTeam1(lineMatches.get(SET_TEAM1));
-                match.setTeam2(lineMatches.get(SET_TEAM2));
-                match.setTossWinner(lineMatches.get(SET_TOSS_WINNER));
-                match.setTossDecision(lineMatches.get(SET_TOSS_DECISION));
-                match.setResult(lineMatches.get(SET_RESULT));
-                match.setDlApplied(lineMatches.get(SET_DL_APPLIED));
-                match.setWinner(lineMatches.get(SET_WINNER));
-                match.setWinByRuns(lineMatches.get(SET_WIN_BY_RUNS));
-                match.setWinByWickets(lineMatches.get(SET_WIN_BY_WICKETS));
-                match.setPlayerOfMatch(lineMatches.get(SET_PLAYER_OF_MATCH));
-                match.setVenue(lineMatches.get(SET_VENUE));
-                if(lineMatches.size()>SET_VENUE+1){
-                        match.setUmpire1(lineMatches.get(SET_UMPIRE1));
+                match.setId(lineMatches.get(ID));
+                match.setSeason(lineMatches.get(MATCH_SEASON));
+                match.setCity(lineMatches.get(MATCH_CITY));
+                match.setDate(lineMatches.get(MATCH_DATE));
+                match.setTeam1(lineMatches.get(MATCH_TEAM1));
+                match.setTeam2(lineMatches.get(MATCH_TEAM2));
+                match.setTossWinner(lineMatches.get(MATCH_TOSS_WINNER));
+                match.setTossDecision(lineMatches.get(MATCH_TOSS_DECISION));
+                match.setResult(lineMatches.get(MATCH_RESULT));
+                match.setDlApplied(lineMatches.get(MATCH_DL_APPLIED));
+                match.setWinner(lineMatches.get(MATCH_WINNER));
+                match.setWinByRuns(lineMatches.get(MATCH_WIN_BY_RUNS));
+                match.setWinByWickets(lineMatches.get(MATCH_WIN_BY_WICKETS));
+                match.setPlayerOfMatch(lineMatches.get(MATCH_PLAYER_OF_MATCH));
+                match.setVenue(lineMatches.get(MATCH_VENUE));
+                if(lineMatches.size()> MATCH_VENUE +1){
+                        match.setUmpire1(lineMatches.get(MATCH_UMPIRE1));
                 }
                 else{
                     match.setUmpire1("");
                 }
-                if(lineMatches.size()>SET_UMPIRE1+1){
-                    match.setUmpire2(lineMatches.get(SET_UMPIRE2));
+                if(lineMatches.size()> MATCH_UMPIRE1 +1){
+                    match.setUmpire2(lineMatches.get(MATCH_UMPIRE2));
                 }
                 else {
                     match.setUmpire2("");
